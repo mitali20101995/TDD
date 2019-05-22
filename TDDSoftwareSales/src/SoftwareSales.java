2484ac8dd18fa51b52d4b4d05da4fae799327e76
Siddharth Trivedi <siddharthtrivedi29@gmail.com>
2019-05-22 11:10:13 -0400

    R1 Red

---
+++ TDDSoftwareSales/src/SoftwareSales.java
@@ -0,0 +1,10 @@
+
+public class SoftwareSales {
+	
+	//returns total number of 
+	public double calculatePrice(int quantity)
+	{
+		return 0.0;
+	}
+
+}
---
+++ TDDSoftwareSales/test/SoftwareSalesTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+class SoftwareSalesTest {
+
+	@Test
+	//R1: Buy one software package
+	public void testBuyOneSoftwarePackage() {
+		
+	}
+	
+	
+}
